295ccc1b5b2ef86a981570104f47dbc80eefce78
garyelephant <garygaowork@gmail.com>
2014-07-18 18:15:15 +0800

    loaded patterns from files

--- pygrok.py
+++ pygrok.py
@@ -1,14 +1,20 @@
 import re
+import os
 
 PATTERN_SUFFIX = '.patt'
 DEFAULT_PATTERNS_DIR = './patterns'
 
+predefined_patterns = {}
+loaded_pre_patterns = False
+"""
 predefined_patterns = {
     'INT'      : r'(?:[+-]?(?:[0-9]+))',
     'WORD'     : r'\b\w+\b',
     'NOTSPACE' : r'\S+',
     'SPACE'    : r'\s*'
 }
+"""
+
 
 class GrokError(Exception):
     pass
@@ -19,14 +25,17 @@ class PatternNotFound(GrokError):
 def grok_match(text, pattern, patterns_dir = DEFAULT_PATTERNS_DIR):
     """
     """
-    _load_patterns(patterns_dir)
+    if loaded_pre_patterns is False:
+        _reload_patterns(patterns_dir)
     py_regex_pattern = re.sub(r'%{(\w+):(\w+)}',
         lambda m: "(?P<" + m.group(2) + ">" + predefined_patterns[m.group(1)] + ")", pattern)
 
     return re.search(py_regex_pattern, text).groupdict()
 
+def _wrap_pattern_name(pat_name):
+    return '%{' + pat_name + '}'
 
-def _load_patterns(patterns_dir):
+def _reload_patterns(patterns_dir):
     """
     BASE10NUM (?<![0-9.+-])(?>[+-]?(?:(?:[0-9]+(?:\.[0-9]+)?)|(?:\.[0-9]+)))
     NUMBER (?:%{BASE10NUM})
@@ -78,7 +87,33 @@ def _load_patterns(patterns_dir):
                                        URI
     
     """
-    pass
+    global predefined_patterns
+    predefined_patterns = {}
+    for f in os.listdir(patterns_dir):
+        patterns = _load_patterns_from_file(f)
+        predefined_patterns.update(patterns)
+
+    global loaded_pre_patterns
+    loaded_pre_patterns = True
+
+
+def _load_patterns_from_file(file):
+    """
+    """
+    patterns = {}
+    with open(file, 'r') as f:
+        for l in f:
+            pat_regex = l.split()
+            pat = Pattern(pat_regex[0], pat_regex[1])
+            pat.sub_patterns = re.findall(_wrap_pattern_name(pat.pattern_name), pat.regex_str)
+            patterns[pat.pattern_name] = pat
+    return patterns
+
+
+def _extract_pattern_names(regex_str):
+    """
+    """
+    re.findall(, regex_str)
 
 
 def _build_pattern_regex(pattern_name, regex_map):
@@ -95,12 +130,12 @@ def _build_pattern_regex(pattern_name, regex_map):
         if pat_name == pattern_name:
             pattern = pattern_temp
 
-        if len(pattern.sub_pattern) > 0 and pat_name != pattern_name:
+        if len(pattern.sub_patterns) > 0 and pat_name != pattern_name:
             pattern.regex_str = _replace_sub_pattern_regex(pattern.regex_str, pat_name, pattern_temp.regex_str)
-            pattern.sub_pattern.remove(pat_name)
+            pattern.sub_patterns.remove(pat_name)
 
-        if len(pattern.sub_pattern) > 0:
-            pat_name = pattern.sub_pattern[0] 
+        if len(pattern.sub_patterns) > 0:
+            pat_name = pattern.sub_patterns[0] 
         else:
             return pattern.regex_str
 
@@ -116,8 +151,8 @@ def _replace_sub_pattern_regex(regex_str, sub_pattern_name, sub_pattern_regex_st
 class Pattern(object):
     """
     """
-    def __init__(self, pattern_name, regex_str, sub_pattern):
+    def __init__(self, pattern_name, regex_str, sub_patterns = {}):
         self.pattern_name = pattern_name
         self.regex_str = regex_str
-        self.sub_pattern = sub_pattern # sub_pattern name list
+        self.sub_patterns = sub_patterns # sub_pattern name list
 
