709e4ba93e486646810a58bc26ab76a7880b07c8
garyelephant <garygaowork@gmail.com>
2014-07-21 17:18:13 +0800

    removed unused codes

--- pygrok.py
+++ pygrok.py
@@ -9,14 +9,6 @@ DEFAULT_PATTERNS_DIR = os.path.dirname(os.path.abspath(__file__)) + '/patterns'
 
 predefined_patterns = {}
 loaded_pre_patterns = False
-"""
-predefined_patterns = {
-    'INT'      : r'(?:[+-]?(?:[0-9]+))',
-    'WORD'     : r'\b\w+\b',
-    'NOTSPACE' : r'\S+',
-    'SPACE'    : r'\s*'
-}
-"""
 
 
 class GrokError(Exception):
@@ -127,40 +119,8 @@ def _load_patterns_from_file(file):
             pat_name = l[:sep]
             regex_str = l[sep:].strip()
             pat = Pattern(pat_name, regex_str)
-            #TODO temp commented pat.sub_patterns = re.findall(_wrap_pattern_name(pat.pattern_name), pat.regex_str)
             patterns[pat.pattern_name] = pat
     return patterns
-
-
-def _build_pattern_regex(pattern_name, regex_map):
-    """
-    """
-    pattern = None
-    pat_name = pattern_name
-    while True:
-        try:
-            pattern_temp = regex_map[pat_name]
-        except KeyError as e:
-            raise PatternNotFound
-
-        if pat_name == pattern_name:
-            pattern = pattern_temp
-
-        if len(pattern.sub_patterns) > 0 and pat_name != pattern_name:
-            pattern.regex_str = _replace_sub_pattern_regex(pattern.regex_str, pat_name, pattern_temp.regex_str)
-            pattern.sub_patterns.remove(pat_name)
-
-        if len(pattern.sub_patterns) > 0:
-            pat_name = pattern.sub_patterns[0] 
-        else:
-            return pattern.regex_str
-
-
-def _replace_sub_pattern_regex(regex_str, sub_pattern_name, sub_pattern_regex_str):
-    """
-    replace sub pattern name with sub pattern regex
-    """
-    return regex_str.replace(_wrap_pattern_name(sub_pattern_name), sub_pattern_regex_str)
 
 
 class Pattern(object):
