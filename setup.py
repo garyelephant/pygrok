e063da5ceb2d44ca4c1b3743ffb99e6f06b44463
garyelephant <garygaowork@gmail.com>
2014-07-23 21:50:53 +0800

    set up setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name = 'pygrok',
+    version = '1.0',
+    description = 'A Python library to parse strings and'
+        + ' extract information from structured/unstructured data',
+    url = 'https://github.com/garyelephant/pygrok',
+    author = 'garyelephant',
+    author_email = 'garygaowork@gmail.com',
+    license = 'MIT',
+    packages = ['pygrok'],
+    include_package_data = True,
+    )
