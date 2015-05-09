ff31a27ea4e278de2db8a75e74ba87805b8f8417
garyelephant <garygaowork@gmail.com>
2015-05-09 07:45:46 +0000

    push pygrok to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'pygrok',
-    version = '1.0',
+    version = '0.5.0',
     description = 'A Python library to parse strings and'
         + ' extract information from structured/unstructured data',
     url = 'https://github.com/garyelephant/pygrok',
@@ -10,4 +10,6 @@ setup(name = 'pygrok',
     license = 'MIT',
     packages = ['pygrok'],
     include_package_data = True,
+    keywords = ['python grok', 'regex'], # arbitrary keywords
+    download_url = 'https://github.com/garyelephant/pygrok/tarball/v0.5.0',
     )
