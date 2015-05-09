290714532dd1efc92a4669418e22c3300c4db8a4
garyelephant <garygaowork@gmail.com>
2015-05-09 08:23:50 +0000

    detect README.rst

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+import os
+
+long_desc = ''
+
+if os.path.exists('README.rst'):
+    long_desc = open('README.rst').read()
 
 setup(name = 'pygrok',
     version = '0.5.0',
     description = 'A Python library to parse strings and'
         + ' extract information from structured/unstructured data',
+    long_description= long_desc,
     url = 'https://github.com/garyelephant/pygrok',
     author = 'garyelephant',
     author_email = 'garygaowork@gmail.com',
